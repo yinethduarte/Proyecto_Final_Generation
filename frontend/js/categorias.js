13e8402c6419a3bbb07de1d28a2f01dc222914a2
AnrubioG <angela.rubio@genstudents.org>
2024-06-06 23:38:28 -0500

    Se agregan filtros para subcategorias

--- frontend/js/categorias.js
+++ frontend/js/categorias.js
@@ -1,49 +1,87 @@
-// Función para obtener y procesar el JSON
-
+// selección del DOM
 const contenedorCategoria = document.querySelector(".categoria");
+const listaSubcategorias = document.querySelector(".lista-subcategorias");
 
+// crear una tarjeta de producto
+function crearCartProducto(product) {
+  const productoHTML = `
+    <div class="contenedor-producto">
+      <div class="header-producto">
+        <h4 class="nombre-producto">${product.nombre}</h4>
+        <p class="precio-producto">${product.precio}</p>
+      </div>
+      <div class="contenedor-img-producto">
+        <img src="https://jousfit.com/cdn/shop/files/456CF653-F41F-488A-B49C-396D8CCAD6D1.jpg?v=1704999170" alt="" />
+      </div>
+      <div class="contenedor-addCart">
+        <button>Add to Cart</button>
+      </div>
+    </div>`;
+  contenedorCategoria.innerHTML += productoHTML;
+}
+
+// filtrar y renderizar productos por SUBCATEGORIA
+function renderizarProductos(productos, subcategoria) {
+  contenedorCategoria.innerHTML = "";
+  if (subcategoria) {
+    const productosFiltrados = productos[subcategoria];
+    if (productosFiltrados) {
+      productosFiltrados.forEach((producto) => {
+        crearCartProducto(producto);
+      });
+    }
+  } else {
+    for (const key in productos) {
+      productos[key].forEach((producto) => {
+        crearCartProducto(producto);
+      });
+    }
+  }
+}
+
+// evento al selecciónar de una subcategoría
+function handleSubcategoriaClick(e, productos) {
+  const subcategoria = e.target.textContent.toLowerCase().replace(/\s+/g, "_");
+  renderizarProductos(productos, subcategoria);
+}
+
+// crear la lista de filtros
+function crearFiltrosSubcategorias(productos) {
+  for (const key in productos) {
+    const subcategoria = key.charAt(0).toUpperCase() + key.slice(1);
+    const itemSubcategoria = `<li class="subcategoria">${subcategoria.replace(
+      /_/g,
+      " "
+    )}</li>`;
+    listaSubcategorias.innerHTML += itemSubcategoria;
+  }
+
+  const subcategorias = document.querySelectorAll(".subcategoria");
+  subcategorias.forEach((sub) => {
+    sub.addEventListener("click", (e) => handleSubcategoriaClick(e, productos));
+  });
+}
+
+// filtrar los productos por CATEGORIA
+function filtrarCategoria(data) {
+  const productos = data.productos; // aca se debe hacer la lógica para que se elija la categoria seleccionada en el nav
+  crearFiltrosSubcategorias(productos);
+  renderizarProductos(productos, null);
+}
+
+// Función para obtener y procesar el JSON
 async function fetchAndPrintJSON() {
   try {
-    // Realizar la solicitud para obtener el archivo JSON local
     const response = await fetch("../json/productos_y_servicios.json");
-    // Verificar si la solicitud fue exitosa
     if (!response.ok) {
       throw new Error("Error al obtener el archivo JSON");
     }
-    // Parsear el contenido del JSON
     const data = await response.json();
-    let productos = data.productos;
-    let servicios = data.servicios;
-
-    function fitlarCategoria(categoria) {
-      for (const key in categoria) {
-        categoria[key].forEach((product) => {
-          let producto = `<div class="contenedor-producto">
-          <div class="header-producto">
-            <h4 class="nombre-producto">${product.nombre}</h4>
-            <p class="precio-producto">${product.precio}</p>
-          </div>
-          <div class="contenedor-img-producto">
-            <img
-              src="https://jousfit.com/cdn/shop/files/456CF653-F41F-488A-B49C-396D8CCAD6D1.jpg?v=1704999170"
-              alt=""
-            />
-          </div>
-          <div class="contenedor-addCart">
-            <button>Add to Cart</button>
-          </div>`;
-
-          contenedorCategoria.innerHTML += producto;
-        });
-      }
-    }
-
-    fitlarCategoria(productos);
+    filtrarCategoria(data);
   } catch (error) {
-    // Manejar cualquier error que ocurra durante la solicitud
     console.error("Hubo un problema con la operación fetch:", error);
   }
 }
 
-// Llamar a la función para obtener y procesar el JSON
+// Llamar a la función principal para obtener y procesar el JSON
 fetchAndPrintJSON();
