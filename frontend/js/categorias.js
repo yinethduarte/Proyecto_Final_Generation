69e3a3bb093b6478af030c48a5a21f2d8d3b8ace
Ribatosis <juliana.espinosa@genstudents.org>
2024-06-25 19:37:38 -0500

    cambios carrito

--- frontend/js/categorias.js
+++ frontend/js/categorias.js
@@ -14,7 +14,7 @@ function crearCardProducto(product) {
         <img src="https://jousfit.com/cdn/shop/files/456CF653-F41F-488A-B49C-396D8CCAD6D1.jpg?v=1704999170" alt="" />
       </div>
       <div class="contenedor-addCart">
-        <button>Add to Cart</button>
+        <button class="add-cart">Add to Cart</button>
       </div>
     </div>`;
   contenedorCategoria.innerHTML += productoHTML;
@@ -85,3 +85,148 @@ async function fetchAndPrintJSON() {
 
 // Llamar a la función principal para obtener y procesar el JSON
 fetchAndPrintJSON();
+
+// Abrir y cerrar carrito
+
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const closeCart = document.querySelector("cart-close");
+
+cartIcon.addEventListener("click",() => {
+  cart.classList.add("active");
+})
+
+closeCart.addEventListener("click", () => {
+  cart.classList.remove("active")
+})
+
+// 
+
+if (document.readyState == 'loading'){
+  document.addEventListener("DOMContentLoaded", start);
+
+}else{
+  start();
+}
+
+// Comenzar
+
+function start (){
+  addEvents();
+}
+
+// Actualizar y volver a presentar
+
+function update (){
+  addEvents();
+  updateTotal();
+}
+
+// Eventos
+
+function addEvents(){
+
+  // Quitar articulo
+
+  let cartRemove_btns = document.querySelectorAll(".cart-remove");
+
+  console.log(cartRemove_btns);
+
+  cartRemove_btns.forEach((btn) =>{
+    btn.addEventListener("click", handle_removeCartItem);
+  })
+
+  // Cambiar cantidad de articulos
+
+  let cartQuantity_inputs = document.querySelectorAll(".cart-quantity");
+
+  cartQuantity_inputs.forEach((input) => {
+    input.addEventListener("change", handle_changeItemQuantity);
+  });
+
+  // Añadir articulos al carrito
+
+  let addCart_btns = document.querySelectorAll(".add-cart");
+
+  addCart_btns.forEach((btn) => {
+    btn.addEventListener("click", handle_addCartItem);
+  });
+}
+
+// Comprar orden
+
+const buy_btn = document.querySelector(".btn-buy");
+buy_btn.addEventListener("click", handle_buyOrder);
+
+// Funciones de manejo de eventos
+
+let itemsAdded = [];
+
+function handle_addCartItem(){
+  let product = this.parentElement;
+  let title = product.querySelector(".nombre-producto").innerHTML;
+  let price = product.querySelector(".precio-producto").innerHTML;
+  let imgSrc = product.querySelector(".contenedor-img-producto").src;
+
+  console.log(title, price, imgSrc)
+}
+
+let newToAdd = {
+  title,
+  price,
+  imgSrc,
+};
+
+// Manejo de elemento ya existente
+
+if(itemsAdded.find((el) => el.title == newToAdd.title)){
+  alert("Este articulo ya existe");
+  return;
+}else{
+  itemsAdded.push(newToAdd);
+}
+
+// Añadir productos al carrito
+
+let cartBoxElement = cartBoxComponent(title, price, imgSrc);
+let newNode = document.createElement("div");
+newNode.innerHTML = cartBoxElement;
+const cartContent = cart.querySelector(".cart-content");
+cartContent.appendChild(newNode);
+
+update();
+
+function handle_removeCartItem(){
+  this.parentElement.remove();
+
+  itemsAdded = itemsAdded.filter{
+    (el) =>
+      el.title != this.parentElement.querySelector(".nombre-producto").innerHTML
+  };
+
+  update();
+}
+
+function handle_changeItemQuantity(){
+  if(isNaN(this.value) || this.value < 1 ){
+    this.value = 1;
+  }
+
+  this.value = Math.floor(this.value); // Mantener el número entero
+
+  update();
+
+}
+
+function handle_buyOrder(){
+  if(itemsAdded.length <= 0){
+    alert("Aun no hay ningun pedido para realizar");
+    return;
+  }
+
+  const cartContent = cart.querySelector("cart-content");
+  cartContent.innerHTML = "";
+  alert("Su pedido se realizó con exito: ");
+  itemsAdded = [];
+  update();
+}
