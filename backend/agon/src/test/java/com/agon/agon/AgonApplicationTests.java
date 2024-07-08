67ee9d70d9fe75acb7e0eaba0ded721d4da2a22f
AnubioG <angela.rubio@genstudents.org>
2024-07-08 00:30:50 -0500

    proyecto inicial con spring

---
+++ backend/agon/src/test/java/com/agon/agon/AgonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agon.agon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
