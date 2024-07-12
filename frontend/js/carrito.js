4ac77d57260ed6b67fc7da9d9400596df8cbf4c6
AnubioG <angela.rubio@genstudents.org>
2024-07-11 21:23:46 -0500

    Agregar productos al carrito

--- frontend/js/carrito.js
+++ frontend/js/carrito.js
@@ -5,25 +5,9 @@ const cart = document.querySelector(".cart");
 const closeCart = document.querySelector("cart-close");
 
 cartIcon.addEventListener("click", () => {
-    cart.classList.add("active");
+  cart.classList.add("active");
 });
 
 closeCart.addEventListener("click", () => {
-    cart.classList.remove("active");
+  cart.classList.remove("active");
 });
-
-// 
-
-if (document.readyState == 'loading'){
-    document.addEventListener("DOMContentLoaded", start);
-}else{
-    start();
-}
-
-
-
-//Comenzar
-
-function start(){
-    addEvents();
-}
