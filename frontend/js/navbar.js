751eede300dce4e462d93636559ccb8719775b13
AnubioG <angela.rubio@genstudents.org>
2024-07-14 02:54:40 -0500

    corrección llamado de productos en carrito

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -15,7 +15,7 @@ async function fetchMenu() {
 
 function agregarAlCarrito(producto) {
   const carrito = JSON.parse(localStorage.getItem("cart")) || [];
-  const productoEnCarrito = carrito.find((p) => p.id == producto.id);
+  const productoEnCarrito = carrito.find((p) => p.nombre == producto.nombre);
 
   if (!productoEnCarrito) {
     producto.cantidad = 1;
@@ -29,11 +29,12 @@ function agregarAlCarrito(producto) {
 
 function eliminarDelCarrito(producto) {
   let carrito = JSON.parse(localStorage.getItem("cart")) || [];
-  const productoEnCarrito = carrito.find((p) => p.id == producto.id);
+  const productoEnCarrito = carrito.find((p) => p.nombre == producto.nombre);
 
   if (productoEnCarrito) {
+    console.log(productoEnCarrito);
     if (productoEnCarrito.cantidad === 1) {
-      carrito = carrito.filter((p) => p.id != producto.id);
+      carrito = carrito.filter((p) => p.nombre != producto.nombre);
     } else {
       productoEnCarrito.cantidad -= 1;
     }
@@ -70,12 +71,14 @@ function renderizarCarrito() {
       minimumFractionDigits: 0,
     }).format(producto.precio * producto.cantidad)}
    </h5>
-    <button class="btn btn-outline-danger" id="decrementar-${
-      producto.id
-    }">-</button>
-    <button class="btn btn-outline-success" id="aumentar-${
-      producto.id
-    }">+</button>
+    <button class="btn btn-outline-danger" style="z-index:9999;" id="decrementar-${producto.nombre.replaceAll(
+      " ",
+      ""
+    )}">-</button>
+    <button class="btn btn-outline-success" style="z-index:9999;" id="aumentar-${producto.nombre.replaceAll(
+      " ",
+      ""
+    )}">+</button>
   </div>
 </div>`;
       contenedorCarrito.innerHTML += productCard;
@@ -103,10 +106,10 @@ function renderizarCarrito() {
     // Añadir eventos
     carrito.forEach((producto) => {
       const decrementarButton = document.querySelector(
-        `button#decrementar-${producto.id}`
+        `button#decrementar-${producto.nombre.replaceAll(" ", "")}`
       );
       const aumentarButton = document.querySelector(
-        `button#aumentar-${producto.id}`
+        `button#aumentar-${producto.nombre.replaceAll(" ", "")}`
       );
 
       decrementarButton.addEventListener("click", () => {
